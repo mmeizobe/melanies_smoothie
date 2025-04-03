8bcc13ba61848d49f13ea8ec2d0d760f5eb904e4
mmeizobe <mmeizaga.ar@gmail.com>
2025-04-03 13:52:05 -0300

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -47,3 +47,10 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         
         st.success('Your Smoothie is ordered, '+ name_on_order , icon="✅")
+
+#new section to display smoothfroot nutrition information
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
+
+
