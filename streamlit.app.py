362161657178c286a336236d071cfe21479116fe
mmeizobe <mmeizaga.ar@gmail.com>
2025-04-03 14:05:38 -0300

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title("🥤Customize Your Smoothie!🥤")
@@ -32,6 +33,9 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width= True)
+
 
     #st.write(ingredients_string)
 
@@ -48,10 +52,7 @@ if ingredients_list:
         
         st.success('Your Smoothie is ordered, '+ name_on_order , icon="✅")
 
-#new section to display smoothfroot nutrition information
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width= True)
+
+
 
 
